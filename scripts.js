3285e53d2ec37e56be6cdefe5761069928871882
nrminar <49846986+nrminar@users.noreply.github.com>
2020-10-13 18:29:27 -0500

    -add class selections

--- scripts.js
+++ scripts.js
@@ -174,17 +174,17 @@ Pathfinder
 Inquisitor
 Heirophant
 Guardian
-Ascendant
-Ascendant
-Ascendant
-Ascendant
-Ascendant
 Ascendant`.split('\n');
 
 function readyNow() {
   $('#randomSkills').on('click', getSkills);
   $('#randomClass').on('click', getClass);
   $('#numberOfSkills').val('1')
+
+  for(a of ascendencies) {
+    $('#classOptions').append(`<button type="checkbox" id="${a}" class="classSelections">${a}</button>`)
+    $(`#${a}`).on('click', selectDeselectClass);
+  }
 }
 
 function getSkills(evt) {
@@ -200,4 +200,15 @@ function getSkills(evt) {
 function getClass(evt) {
   $('#classResults').empty();
   $('#classResults').append(`<div id="class">${ascendencies[Math.floor(Math.random() * ascendencies.length)]}</div><img src="https://i.imgflip.com/4ie36w.jpg">`);
+}
+function selectDeselectClass(evt) {  
+  $(evt.target).toggleClass('classDeSelections');
+  $(evt.target).toggleClass('classSelections');
+  let name = evt.target.id;
+
+  if(ascendencies.includes(name)){
+    ascendencies.splice(ascendencies.indexOf(name), 1);
+  } else {
+    ascendencies.push(name)
+  }
 }
